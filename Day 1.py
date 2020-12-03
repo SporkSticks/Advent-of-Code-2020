203a68c703986c8e534fc217869a9ddae1525bce
spookysauces <74909142+spookysauces@users.noreply.github.com>
2020-12-03 14:44:29 +1100

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,54 @@
+day1_file = open("C:/Users/zstavrakas/Desktop/Advent of Code 2020/Input Files/1.1.txt", 'r')
+
+# put all the entries in a single list of integer values
+entries = []
+for item in day1_file:
+    entries.append(int(item))
+
+# PART 1
+# Find 2 numbers in the list whose sum is 2020 and return their product
+def twentytwenty_2(entry_list):
+    i = 0
+
+    for number in entry_list:
+        i += 1
+        for num_to_test in entry_list[i:]:
+            if number + num_to_test == 2020:
+                answer = number * num_to_test
+                return answer
+
+    return "No two values in the list added up to 2020, try again!"
+
+# PART 2
+# Find 3 numbers in the list whose sum is 2020 and return their product
+def twentytwenty_3(entry_list):
+    i = 0
+
+    for a in entry_list:
+        i += 1
+        copy_list = entry_list[i:]
+        for b in entry_list[i:]:
+            if a + b >= 2020:
+                continue
+            else:
+                copy_list.remove(b)
+                for c in copy_list:
+                    if a + b + c == 2020:
+                        return a * b * c
+    
+    return "No three values in the list added up to 2020, try again!"
+
+# notes
+# for testing 3 numbers, if a + b >= 2020 --> do not test for a third number
+# there are a lot of valid combos to work through, you want to avoid repetitions, shrink the list, avoid self-counting a/b during c-check
+
+
+# test fucntion + cases
+print("Testing 2 Int Sums...")
+print(twentytwenty_2([1, 2, 3, 4, 5, 6, 2021, 2022, 1980, 1999, 2016]))  # should return 8064
+print(twentytwenty_2([1721, 979, 366, 299, 675, 1456])) # should return 514579
+print(twentytwenty_2(entries)) 
+print("Testing 3 Int Sums...")
+print(twentytwenty_3([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2020, 2018])) # should return 2018
+print(twentytwenty_3([1721, 979, 366, 299, 675, 1456])) # should return 241861950
+print(twentytwenty_3(entries)) 
---
+++ Day 2.py
@@ -0,0 +1,68 @@
+day2_file = open("C:/Users/zstavrakas/Desktop/Advent of Code 2020/Input Files/2.1.txt", 'r')
+
+# put all the passwords into a single list
+raw_passwords = day2_file.read()
+password_list = raw_passwords.split('\n')
+
+# PART 1
+# checks if a given password is valid by checking if the number of times the character appears lays within the range (min-max inclusive)
+def password_check1(min_num, max_num, character, password):
+    return min_num <= password.count(character) <= max_num
+
+
+# tallies up the number of correct passwords from a list by passing the organised values into the password_check function
+# each password is in the form --> "min-max letter: password"
+def pass_counter1(passwords):
+    total = 0
+
+    for password_data in passwords:
+        split_data = password_data.split()
+        password = split_data[2]
+        character = split_data[1][0]
+        
+        range_data = split_data[0].split("-")
+        min_num, max_num = int(range_data[0]), int(range_data[1])
+
+        total += password_check1(min_num, max_num, character, password)
+
+    return total
+
+
+# PART 2
+# checks if a given password has **exactly** one character in the one-indexed positions provided
+# e.g. 1-3 a: abcde is valid --> 'a' is in position 1 but not 3
+
+def password_check2(pos1, pos2, character, password):
+    pass_len = len(password)
+
+    if pos1 > pass_len or pos2 > pass_len:
+        return 0
+
+    pos1true = password[pos1 - 1] == character
+    pos2true = password[pos2 - 1] == character
+
+    return pos1true ^ pos2true
+
+def pass_counter2(passwords):
+    total = 0
+
+    for password_data in passwords:
+        split_data = password_data.split()
+        password = split_data[2]
+        character = split_data[1][0]
+        
+        range_data = split_data[0].split("-")
+        pos1, pos2 = int(range_data[0]), int(range_data[1])
+
+        total += password_check2(pos1, pos2, character, password)
+
+    return total
+
+
+# test function + cases
+print(pass_counter1(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]))  # should return 2
+print(pass_counter1(password_list))
+
+print(pass_counter2(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"])) # should return 1
+print(pass_counter2(["3-11 b: adbc"])) # should return 1
+print(pass_counter2(password_list))
